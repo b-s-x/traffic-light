c0c4d963b01a18747ae1e0dbe48e11a4130dbac9
b-s-x <b.s.x.mars@gmail.com>
2021-03-21 12:20:46 +0300

    small style fix

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const app = express()
 const path = require('path')
 const fs = require('fs');
 
-process.env.NODE_ENV == 'development'
-
 const port = process.env.PORT || '8000'
 
 app.use(express.static(`${__dirname}/dist`))
-
 
 app.use('*', (req, res) => {
   const indexPath = path.resolve(__dirname, './dist/index.html');
